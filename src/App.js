44a99a9e1f87bd239f012b33ba199549f0a9a927
lorijr <jrsilva.lorivas@gmail.com>
2026-05-22 21:35:48 -0300

    entendendo JSX

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function sum(a,b){
+    return a+b
+  }
+
+  const url = 'https://images.pexels.com/photos/32140540/pexels-photo-32140540.jpeg'
+
+  const name = "Lou Junior"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Alterando o JSX</h2>
+      <p>Olá {name}</p>
+      <p>{sum(10,15)}</p>
+      <img src={url} alt='Imagem' width="150" height="150"/>
+
+
     </div>
   );
 }
