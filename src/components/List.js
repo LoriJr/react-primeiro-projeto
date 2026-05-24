5f087e70e8126a0572609adedd868a7f6ef3d193
lorijr <jrsilva.lorivas@gmail.com>
2026-05-23 22:40:43 -0300

    usando avançado de props

--- src/components/List.js
+++ src/components/List.js
@@ -5,9 +5,9 @@ function List(){
     return(
         <>
             <ul>
-                <Item marca="Ferrari"/>                
-                <Item marca="Maserati"/>                
-                <Item marca="Bugati"/>                
+                <Item marca="Ferrari" ano_lancamento={1985}/>                
+                <Item marca="Maserati" ano_lancamento={1950}/>                
+                <Item marca="Bugati" ano_lancamento={1964}/>
             </ul>
         </>
     )
