a4ede28b176dd8611e0acdd4ced9cda4e8f9c461
lorijr <jrsilva.lorivas@gmail.com>
2026-05-27 16:19:14 -0300

    renderização de listas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-import Evento from './components/Evento';
-import Form from './components/Form';
-import Condicional from './components/Condicional';
+import NovaLista from './components/NovaLista';
 
 function App() {
+
+  const meusItens = ['Angular','React', 'Vue']
+
   return(
-    <div>
-      <h1>Renderização Condicional</h1>
-      <Condicional />
+    <div className="App">
+      <h1>Renderização de Listas</h1>      
+      <NovaLista itens={meusItens}/>
+      <NovaLista itens={[]}/>
     </div>
   ) 
 }
---
+++ src/components/NovaLista.js
@@ -0,0 +1,16 @@
+function NovaLista({itens}){
+
+    return(      
+      <>
+        <h3>Lista de Itens</h3>
+        {itens.length > 0 ? (
+            itens.map((item, index)=>(
+                <p key={index}>{item}</p>
+            ))) : (
+            <p>Lista vazia</p>)}
+      </>
+        
+    )
+}
+
+export default NovaLista
